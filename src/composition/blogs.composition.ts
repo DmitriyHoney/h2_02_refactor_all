dc5e50a8f9c651be555058f4ca2ed0d8b32b76f8
Bolshakov Dmitry <eur719@gmail.com>
2023-05-17 12:34:10 +0300

    fixed composition blogs

--- src/composition/blogs.composition.ts
+++ src/composition/blogs.composition.ts
@@ -2,10 +2,15 @@ import { Container } from 'inversify';
 import {BlogsControllers} from "../controllers/blogs.controllers";
 import {BlogsService} from "../services/blogs.services";
 import {BlogsCommandRepo, BlogsQueryRepo} from "../repositry/blogs.repositry";
+import { PostsService } from '../services/posts.services';
+import { PostsCommandRepo, PostsQueryRepo } from '../repositry/posts.repositry';
 
 const container = new Container();
 container.bind(BlogsControllers).to(BlogsControllers);
 container.bind(BlogsService).to(BlogsService);
+container.bind(PostsService).to(PostsService);
+container.bind(PostsQueryRepo).to(PostsQueryRepo);
+container.bind(PostsCommandRepo).to(PostsCommandRepo);
 container.bind(BlogsQueryRepo).to(BlogsQueryRepo);
 container.bind(BlogsCommandRepo).to(BlogsCommandRepo);
 export default container;
