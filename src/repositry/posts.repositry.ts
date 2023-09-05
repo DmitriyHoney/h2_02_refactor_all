502d7b4f8ee8992480123df4478209ea753d630e
Dmitry Bolshakov <eur719@gmail.com>
2023-09-05 14:42:37 +0300

    fixed like-status

--- src/repositry/posts.repositry.ts
+++ src/repositry/posts.repositry.ts
@@ -35,7 +35,7 @@ function postMap(i: any, userId: string | undefined) {
     const userStatus = i.extendedLikesInfo?.newestLikes.find((u: any) => u.userId === userId && u.status);
     const myStatus = userStatus ? userStatus.status : Likes.NONE;
     const newestLikes = i.extendedLikesInfo?.newestLikes
-        .filter((i: any) => i.status === Likes.LIKE) // && i.userId !== userId
+        // .filter((i: any) => i.status === Likes.LIKE) // && i.userId !== userId
         .map((i: any) => {
             return {
                 addedAt: i.addedAt,
